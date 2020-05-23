fcd4d050b2ab857e4c5f8a8ced59e3a6212eb68e
SmallOyster <master@xshgzs.com>
2020-05-23 22:39:16 +0800

    test git

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -29,12 +29,9 @@ class Index extends BaseController
 			return redirect('/index/logout');
 		}
 
-		$token = sha1(time() . mt_rand());
-		Session::set('token', $token);
-
 		return view('index', [
 			'staticPath' => Config::get('app.app_host'),
-			'token' => $token
+			'token' => createToken()
 		]);
 	}
 
@@ -130,8 +127,10 @@ class Index extends BaseController
 	{
 		$key = inputGet('key', 0);
 
-		if ($key === 'test') {
-			die(header('location: https://www.baidu.com'));
+		if ($key === '2581979df3657a27664cc02aeb9d1083') {
+			die(header('location: https://twitter.com/tt2020joyster/status/1258984341222199296?s=09'));
+		}	elseif ($key === '59c4a212f12e419e9bb4a15c69e0a889') {
+			die(header('location: https://www.xtube.com/profile/jip1006-29480901'));
 		} else {
 			return view('common@/error', ['status' => 'error', 'errorTips' => '非法外链key<br>如有疑问，请联系技术支持']);
 		}
